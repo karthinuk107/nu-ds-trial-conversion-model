65bc6ae9e0584dda6910345515e36785d488521b
karthinuk107 <karthi.vincent@news.co.uk>
2017-11-15 18:07:02 +0530

    Adding some queries

--- queries/omniture_agg.sql
+++ queries/omniture_agg.sql
@@ -4,7 +4,7 @@ SELECT
 FROM
   `newsuk-datatech-dev-1251.tnl_trial_conversion.conversion_base_90days` a
 LEFT JOIN
-  `newsuk-datatech-dev-1251.tnl_trial_conversion.adhoc.trial` b
+  `newsuk-datatech-dev-1251.tnl_trial_conversion.omniture_history` b
 ON
   a.cpn = b.cpn
   AND DATE(a.timestamp) = DATE(TIMESTAMP (b.activity_date))
--- queries/omniture_agg_base.sql
+++ queries/omniture_agg_base.sql
@@ -1,34 +1,35 @@
-select* from (SELECT
+select * from (SELECT
   cpn,
   country,
   gender,
   CASE
-    WHEN dob <> '' THEN YEAR(timestamp)-INTEGER(LEFT(dob,4))
+    WHEN dob <> '' THEN YEAR(TIMESTAMP)-INTEGER(LEFT(dob, 4))
   END AS age,
   subscriptions_contractStartDate,
+  subscriptions_subscriptionStatusCode,
+  CancellationRequestedDate,
   subscriptions_promoCode,
   subscriptions_paymentMethodSelection,
-  subscriptions_CancellationRequestedDate,
-  NVL(duration_cancellation ,0) as duration_cancellation ,
+  NVL(duration_cancellation, 0) AS duration_cancellation,
   status,
-  SUM(active_days) AS active_days,
-  SUM(recency) AS recency,
-  SUM(events_weekdays) AS events_weekdays,
-  SUM(events_weekends) AS events_weekends,
-  SUM(active_weekdays) AS active_weekdays,
-  SUM(active_weekends) AS active_weekends,
-  SUM(active_weeks) AS active_weeks,
-  SUM(visits_weekdays) AS visits_weekdays,
-  SUM(visits_weekends) AS visits_weekends,
-  SUM(views_weekdays) AS views_weekdays,
-  SUM(views_weekends) AS views_weekends,
-  SUM(morning_views) AS morning_views,
-  SUM(afternoon_views) AS afternoon_views,
-  SUM(evening_views) AS evening_views,
-  SUM(late_night_views) AS late_night_views,
-  SUM(total_page_views) AS total_page_views,
+  SUM(active_days ) AS active_days,
+  SUM(recency ) AS recency,
+  SUM(events_weekdays ) AS events_weekdays,
+  SUM(events_weekends ) AS events_weekends,
+  SUM(active_weekdays ) AS active_weekdays,
+  SUM(active_weekends ) AS active_weekends,
+  SUM(active_weeks ) AS active_weeks,
+  SUM(visits_weekdays ) AS visits_weekdays,
+  SUM(visits_weekends ) AS visits_weekends,
+  SUM(views_weekdays ) AS views_weekdays,
+  SUM(views_weekends ) AS views_weekends,
+  SUM(morning_views ) AS morning_views,
+  SUM(afternoon_views ) AS afternoon_views,
+  SUM(evening_views ) AS evening_views,
+  SUM(late_night_views ) AS late_night_views,
+  SUM(total_page_views ) AS total_page_views,
 FROM
-  [newsuk-datatech-dev-1251:tnl_trial_conversion.omniture_history]
+  [newsuk-datatech-dev-1251:tnl_trial_conversion.omniture_agg]
 WHERE
   duration_today <= 45
 GROUP BY
@@ -36,12 +37,12 @@ GROUP BY
   country,
   gender,
   age,
+  status,
   subscriptions_contractStartDate,
+  subscriptions_subscriptionStatusCode,
+  CancellationRequestedDate,
   subscriptions_promoCode,
   subscriptions_paymentMethodSelection,
-  subscriptions_CancellationRequestedDate,
-  status,
   primary_device,
-   duration_cancellation ,
-  secondary_device)
-  where cpn='AAAA013516076'
+  duration_cancellation,
+  secondary_device) where cpn ='JBC6842132868'
