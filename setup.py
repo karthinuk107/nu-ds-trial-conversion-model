47716c872fe0ee1c02f7ea1da9b970f88065e519
karthinuk107 <karthi.vincent@news.co.uk>
2017-11-15 14:17:02 +0530

    Adding Setup.py files

---
+++ setup.py
@@ -0,0 +1,12 @@
+from distutils.core import setup
+
+setup(
+    name='Trial_conversion_v1',
+    version='0.0.1',
+    packages=['lifelines'],
+    url='',
+    license='',
+    author='KARTHI VINCENT',
+    author_email='karthi.vincent@news.co.uk',
+    description='Estimating cancellation rarte over time'
+)
