c22bc5428787619c0b764711f9e8e7c6e5d8ad18
karthinuk107 <karthi.vincent@news.co.uk>
2017-12-14 12:04:11 +0530

    Updating Conversion base table

--- queries/conversion_base_90days.sql
+++ queries/conversion_base_90days.sql
@@ -1,14 +1,14 @@
 SELECT
-  *
+  *,
+  CASE
+    WHEN current_status <> 'Active' THEN ABS(CAST(DATE(CAST(subscriptions.contractStartDate1 AS TIMESTAMP)) AS DATE)-CAST(DATE(COALESCE(CancellationRequestedDate,contractEndDate)) AS DATE))
+    ELSE integer ('0')
+  END AS duration_cancellation
 FROM (
   SELECT
     *,
     ROW_NUMBER() OVER (PARTITION BY cpn, TIMESTAMP) ROW_NUMBER,
     ABS(CAST(DATE(CAST(subscriptions.contractStartDate AS TIMESTAMP)) AS DATE)-CAST(DATE(TIMESTAMP) AS DATE)) AS duration_today,
-    CASE
-      WHEN current_status <> 'Active' THEN ABS(CAST(DATE(CAST(subscriptions.contractStartDate1 AS TIMESTAMP)) AS DATE)-CAST(DATE(COALESCE(CancellationRequestedDate,contractEndDate)) AS DATE))
-      ELSE integer ('0')
-    END AS duration_cancellation,
     CASE
       WHEN subscriptions.subscriptionStatusCode =='Active' THEN 1
       ELSE 0
@@ -16,11 +16,17 @@ FROM (
     CASE
       WHEN subscriptions.subscriptionStatusCode =='Active' THEN 0
       ELSE 1
-    END AS status_cancellation
+    END AS status_cancellation,
+    MAX(CASE
+        WHEN subscriptions_CancellationRequestedDate_1 IS NULL THEN subscriptions.CancellationRequestedDate
+        ELSE NULL
+      END ) OVER (PARTITION BY cpn) AS CancellationRequestedDate
   FROM (
     SELECT
       *,
-      MAX(subscriptions.CancellationRequestedDate ) OVER (PARTITION BY cpn) AS CancellationRequestedDate,
+      MIN(CASE
+          WHEN subscriptions.subscriptionStatusCode = 'Cancellation Requested' AND DATE(timestamp) <= '2017-04-18' THEN DATE(timestamp)
+          ELSE NULL END) OVER (PARTITION BY cpn) AS subscriptions_CancellationRequestedDate_1,
       MAX(CASE
           WHEN DATE(subscriptions.contractEndDate) = '9999-12-31' THEN NULL
           ELSE subscriptions.contractEndDate
@@ -34,8 +40,7 @@ FROM (
     WHERE
       TIMESTAMP <= TIMESTAMP(DATE_ADD(subscriptions.contractStartDate,90,"DAY"))
       AND subscriptions.mpc = 'MP370'
-      AND subscriptions.subscriptionStatusCode IN ('Active',
-        'Cancelled',
+      AND subscriptions.subscriptionStatusCode IN ( 'Cancelled',
         'Terminated',
         'Cancellation Requested') ) )
 WHERE
