449f11b0b8d9202982b073b850943cafe009ffbd
karthinuk107 <karthi.vincent@news.co.uk>
2018-01-06 11:24:53 +0530

    Updating Queries with Proper Folder Structure

--- queries/trial_agg_page_views_31days.sql
+++
@@ -1,9 +0,0 @@
-SELECT
-  a.*,
-  b.* EXCEPT(cpn)
-FROM
-  `newsuk-datatech-dev-1251.tnl_trial_conversion_mid_term.trial_agg_31days` a
-LEFT JOIN
-  `newsuk-datatech-dev-1251.tnl_trial_conversion_mid_term.page_views_trend_31days` b
-ON
-  a.cpn = b.cpn
--- queries/trial_master_feature_31days.sql
+++
@@ -1,9 +0,0 @@
-SELECT
-  a.*,
-  b.* EXCEPT(cpn)
-FROM
-  `newsuk-datatech-dev-1251.tnl_trial_conversion_mid_term.trial_agg_page_views_31days` a
-LEFT JOIN
-  `newsuk-datatech-dev-1251.tnl_trial_conversion.athena_axiom_features` b
-ON
-  a.cpn = b.cpn
